84087c6428ffea4ec90108ef39e34be63c7c6b9e
wwalpha <wwalpha@gmail.com>
2020-08-17 18:39:39 +0900

    update

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_sharp_example
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # c-sharp-example
+
+## Create New Project
+
+`dotnet new console`
+
+## Execute Code
+
+`dotnet run`
